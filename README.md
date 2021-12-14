003666d1ea6f631e8001e3f4e26b06b7a0fcea1b
wx1458084829 <wx1999125>
2021-12-14 23:51:22 +0800

    19题：删除倒数第k个元素

--- README.md
+++ README.md
@@ -26,28 +26,86 @@
 
 
 
-# 数组
+# 链表
 
 链表是改善数组的插入和删除
 
 未知数据量
 
-
+## 单链表
 
 链表 插入和删除
 
-双向链表
+
+
+### 经典问题
+
+#### 删除倒数第k个节点
+
+**双指针解法或者快慢指针**
+
+实质上类似于做了一个减法，我们设从后往前数为倒序号，从前往后为正序号，那么 `倒序号(1为起始下标)+正序号(1为起始下标)-1=总长度` ，由此公式可得：
+
+1. fast指针首先移动k个位置。
+2. slow指针和fast指针也一起移动，移动的基准为fast指针到底。
+3. 那么slow指针就刚好到我们要删除的元素的前一个位置。
+4. 以slow为基准，执行链表删除操作即可。
+
+> 由于链表具有头节点，那么上面的公式不应该减一，相当于正序号是从0开始的。
+
+![请添加图片描述](https://img-blog.csdnimg.cn/7016574c6d9a4a44988bc3f5a955e3fb.png?x-oss-process=image/watermark,type_d3F5LXplbmhlaQ,shadow_50,text_Q1NETiBAd3gxNDU4MDg0ODI5,size_20,color_FFFFFF,t_70,g_se,x_16)
+
+**代码如下：**
+
+````java
+		/**
+     * 19. 删除链表的倒数第 N 个结点
+     * 删除链表的倒数第N个节点(双指针，即快慢指针)
+     * 注意：考虑删除的位置
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        ListNode slow = head;
+        //fast移n步，目的是为了判断删除的位置
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        //如果fast为空，表示删除的是头结点
+        if (fast == null) {
+            return head.next;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        //当前slow为要删除的第k个节点第前一个节点
+        slow.next = slow.next.next;
+        return head;
+    }
+````
+
+#### 合并两个有序链表
+
+#### 回文链表
+
+#### 反转链表
+
+#### 反转相邻节点链表
+
+#### 判断链表是否有环
+
+**利用set **
+
+**快慢指针**
+
+## 循环链表
+
+## 双向链表
 
 
 
-反转链表
-
-反转相邻节点链表
-
-
-
-判断链表是否有环
-
-利用set o（n）
-
-快慢指针
--- src/com/namo/practice/Solution.java
+++ src/com/namo/practice/Solution.java
@@ -3,35 +3,43 @@ package com.namo.practice;
 import org.junit.Test;
 
 public class Solution {
-    /**
-     * 数组转换成链表
-     *
-     * @param arr 数组
-     * @return 返回对应带头节点的链表
-     */
-    public static ListNode covertArr2LinkedList(int[] arr) {
-        ListNode head = new ListNode();
-        ListNode tag = head;
-        for (int i = 0; i < arr.length; i++) {
-            ListNode temp = new ListNode(arr[i]);
-            temp.next = null;
-            tag.next = temp;
-            tag = tag.next;
-        }
-        return head;
+
+    @Test
+    public void removeNthFromEndTest() {
+        ListNode listNode = covertArr2LinkedList(new int[]{1, 2, 3, 4, 5, 6});
+        this.removeNthFromEnd(listNode, 4);
+        showLinkedList(listNode);
     }
 
+
     /**
-     * 输出带头节点链表带内容
+     * 19. 删除链表的倒数第 N 个结点
+     * 删除链表的倒数第N个节点(双指针，即快慢指针)
+     * 注意：考虑删除的位置
      *
      * @param head
+     * @param n
+     * @return
      */
-    public static void showLinkedList(ListNode head) {
-        while (head.next != null) {
-            head = head.next;
-            System.out.print(head.val + " ");
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        ListNode slow = head;
+        //fast移n步，目的是为了判断删除的位置
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
         }
-        System.out.println("end");
+        //如果fast为空，表示删除的是头结点
+        if (fast == null) {
+            return head.next;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        //当前slow为要删除的第k个节点第前一个节点
+        slow.next = slow.next.next;
+        return head;
     }
 
 
@@ -40,6 +48,7 @@ public class Solution {
         ListNode listNode = covertArr2LinkedList(new int[]{1, 2, 3, 4, 5});
         showLinkedList(listNode);
     }
+
 
     /**
      * 合并两个有序链表
@@ -72,5 +81,36 @@ public class Solution {
             }
         }
         return listNode.next;
+    }
+
+    /**
+     * 数组转换成链表
+     *
+     * @param arr 数组
+     * @return 返回对应带头节点的链表
+     */
+    public static ListNode covertArr2LinkedList(int[] arr) {
+        ListNode head = new ListNode();
+        ListNode tag = head;
+        for (int i = 0; i < arr.length; i++) {
+            ListNode temp = new ListNode(arr[i]);
+            temp.next = null;
+            tag.next = temp;
+            tag = tag.next;
+        }
+        return head;
+    }
+
+    /**
+     * 输出带头节点链表带内容
+     *
+     * @param head
+     */
+    public static void showLinkedList(ListNode head) {
+        while (head.next != null) {
+            head = head.next;
+            System.out.print(head.val + " ");
+        }
+        System.out.println("end");
     }
 }
