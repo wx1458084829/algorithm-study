fc4a8552dcc5925adb62c1a53efb8cf24ccddcbd
wx1458084829 <15328400570@163.com>
2022-03-31 20:21:40 +0800

    新增测试文件

---
+++ src/com/namo/temp/Main.java
@@ -0,0 +1,14 @@
+package com.namo.temp;
+
+import org.junit.Test;
+
+/**
+ * @author namo
+ * @date 2022/3/31-20:19
+ */
+public class Main {
+    @Test
+    public void mainTest(){
+        System.out.println("这是临时测试代码！");
+    }
+}
