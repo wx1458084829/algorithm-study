ac6fc649a6aec5f4d6982805558b1e6b1b34aee9
wx1458084829 <15328400570@163.com>
2022-04-11 16:42:23 +0800

    判断输出结果 1.运算优先级是先算long相加，然后再拼接字符 2.函数没有static的话作用位置是对象

--- src/com/namo/temp/Main.java
+++ src/com/namo/temp/Main.java
@@ -11,4 +11,18 @@ public class Main {
     public void mainTest(){
         System.out.println("这是临时测试代码！");
     }
+    @Test
+    public void addTest(){
+        long a[] = {1,2,3};
+        long b[] = {4,5,6};
+        b = addFn(b);
+        // 注意运算顺序
+        System.out.print(a[0]+a[1]+a[2]+" ");
+        System.out.println(b[0]+b[1]+b[2]);
+    }
+
+    public long[] addFn(long[] c){
+        c[1] = 100;
+        return c;
+    }
 }
